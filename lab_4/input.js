229a407b49f1d55d4a74027ca62772379ab9b002
unknown <rusakovevgn@yandex.ru>
2024-11-15 12:44:46 +0300

    sgdfg

---
+++ lab_4/input.js
@@ -0,0 +1,141 @@
+function getTarget(e) {
+    var targ
+
+    if (!e) var e = window.event;
+    if (e.target) targ = e.target;
+    else if (e.srcElement) targ = e.srcElement;
+    if (targ.nodeType == 3) // defeat Safari bug
+        targ = targ.parentNode;
+
+    return targ;
+}
+
+function restorePosition(targ, lastX, lastY) {
+    targ.style.top = lastX;
+    targ.style.left = lastY;
+}
+
+let expired;
+var isActive = false;
+var isDblTouched = false;
+var isColorChangeActive = false;
+var targ;
+var lastX, lastY;
+
+let doubleClick = function () {
+    console.log('double click')
+    isDblTouched = true;
+}
+
+let doubleTouch = function (e) {
+    if (e.touches.length === 1) {
+        if (!expired) {
+            expired = e.timeStamp + 400
+        } else if (e.timeStamp <= expired) {
+            e.preventDefault()
+            doubleClick()
+            expired = null
+        } else {
+            expired = e.timeStamp + 400
+        }
+    }
+}
+
+function prepareDivs() {
+    const Divs = document.getElementsByClassName("target");
+
+    for (let i = 0; i < Divs.length; i++) {
+        Divs[i].addEventListener('mousedown', function(e) {
+            targ = getTarget(e);
+            lastX = targ.style.top;
+            lastY = targ.style.left;
+
+            isActive = true;
+            offset = [
+                targ.offsetLeft - e.clientX,
+                targ.offsetTop - e.clientY
+            ];
+        }, true);
+
+        Divs[i].addEventListener("dblclick", function(e) {
+            targ = getTarget(e);
+            lastX = targ.style.top;
+            lastY = targ.style.left;
+            isActive = true;
+            isColorChangeActive = true;
+        });
+
+        Divs[i].addEventListener('mouseup', function() {
+            isActive = false;
+            isColorChangeActive = false;
+        }, true);  
+
+        Divs[i].addEventListener("touchstart", function(e) {
+            if (isDblTouched == false) {
+                targ = getTarget(e);
+                lastX = targ.style.top;
+                lastY = targ.style.left;
+
+                isActive = true;
+                offset = [
+                    targ.offsetLeft - e.touches[0].clientX,
+                    targ.offsetTop - e.touches[0].clientY
+                ];
+            }
+        });
+
+        Divs[i].addEventListener("touchend", function(e) {
+            if (isDblTouched == false) {
+                isActive = false;
+            } 
+        });
+
+        Divs[i].addEventListener("touchstart", doubleTouch);
+
+    }
+
+    document.addEventListener('keydown', function(e) {
+        if (e.key === "Escape") {
+            isActive = false;
+            isColorChangeActive = false;
+            restorePosition(targ, lastX, lastY);
+        }
+    });
+
+    document.addEventListener('mousemove', function(e) {
+        if (isActive) {
+            mousePosition = {
+                x : e.clientX,
+                y : e.clientY
+            };
+
+            targ.style.left = (mousePosition.x + offset[0]) + 'px';
+            targ.style.top  = (mousePosition.y + offset[1]) + 'px';
+        }      
+    }, true);
+
+    document.addEventListener('mousemove', function(e) {
+        if (isColorChangeActive) {
+            x = e.clientX;
+            y = e.clientY;
+            red = x;
+            green = y;
+            blue = (x + y) / 2; 
+            color = [red, green, blue].join(", ");
+            targ.style.backgroundColor = 'rgb(' + color + ')';
+        }      
+    }, true);
+
+    document.addEventListener("touchmove", function(e) {
+        if (isActive == true) {
+            x = e.touches[0].clientX;
+            y = e.touches[0].clientY;
+
+
+            targ.style.left = (x + offset[0]) + 'px';
+            targ.style.top  = (y + offset[1]) + 'px';
+        }  
+    });
+}
+
+prepareDivs();
